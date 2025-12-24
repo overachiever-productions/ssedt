4b51dadba1ab5fe54a6258e19a365be9b175042a
Michael K. Campbell <mike@overachiever.net>
2025-12-24 11:31:57 -0800

    SSEDT-5: Additional stubs for testing/etc.

--- ssedt.psm1
+++ ssedt.psm1
@@ -2,7 +2,38 @@
 # PUBLIC:
 # ==================================================================================================================================	
 function Enable-AutoStartForEphemeralDisks {
+	[CmdletBinding()]
+	# hmm... do I want to let the USER specify a location for the 'InvokeEphemeralDisksSetup.ps1' script? 
+	# 		probably. i.e., default it to ... C:\PerfLogs? or maybe <path-to-sql-server-stuff>? 
+	# 		but ... let users specify an entire path? 
+	param (
+		
+	);
 	
+	begin {
+		
+	};
+	
+	process {
+		
+		
+		# get user inputs... 
+		
+		# load `__invocation_template.ps1`
+		# replace params/details as needed and persist as: 
+		# 		<path>\InvokeEphemeralDisksSetup.ps1
+		
+		# destroy job if exists. 
+		
+		# create a job to execute <path>\InvokeEphemeralDisksSetup.ps1 upon startup
+		# 		with all of the necessary switches. 
+		# 		including -Verbose - i.e., any 'automated' call to Set-EphemeralDisks will always pass in -Verbose... 
+		
+	};
+	
+	end {
+		
+	};
 }
 
 # aliases might be Initialize-EphemeralDisks... and Mount-EphemeralDisks 
