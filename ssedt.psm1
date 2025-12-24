aa9fc28d95e34ab7aa41a66efdd5a325da98d8f6
Michael K. Campbell <mike@overachiever.net>
2025-12-24 11:22:38 -0800

    SSEDT-5: SUPER-BASIC LOGGING done.
    This 'logging' would be a joke in most cases as it's just PowerShell's transcript functionality.
    ONLY. Since the FUNC in question that 'logs stuff' is going to be called by Windows Task Scheduler... this 'functionality' will let me see if I'm correctly wiring up task scheduler to run .ps1 files ... when the OS starts.
    i.e., powershell code/logic is ... easy. involving windows task scheduler - while it should be straight-forward, is always full of fun surprises and hiccups.

--- ssedt.psm1
+++ ssedt.psm1
@@ -1,8 +1,39 @@
 ﻿# ==================================================================================================================================
 # PUBLIC:
 # ==================================================================================================================================	
+function Enable-AutoStartForEphemeralDisks {
+	
+}
 
-
+# aliases might be Initialize-EphemeralDisks... and Mount-EphemeralDisks 
+function Set-EphemeralDisks {
+	[CmdletBinding()]
+	param (
+		[Alias('Volumes')]
+		[Parameter(Mandatory)]
+		[string[]]$TempDbVolumes,
+		[Alias('Instance', 'InstanceName')]
+		[string]$SqlInstanceName = "MSSQLSERVER",
+		[string]$TempDbDirectoryName = "sqltemp"
+	);
+	
+	begin {
+		$transcriptFile = "C:\Windows\Temp\ssedt_transcript_$(Get-Date -Format "yyyy-mm-dd_hhMMss").txt";
+		Start-Transcript -Path $transcriptFile;
+		Write-Host "Transcript Started. Location: [$transcriptFile]";
+	};
+	
+	process {
+		Write-Host "Parameters: ";
+		Write-Host "`tVolumes: [$TempDbVolumes]";
+		Write-Host "`tInstance: [$SqlInstanceName]";
+		Write-Host "`tTempDbDir: [$TempDbDirectoryName]";
+	};
+	
+	end {
+		
+	};
+}
 
 # ==================================================================================================================================
 # INTERNAL:
